26c9bfd64f8aa4db9fd539c1eeea9b1c213dcd1e
Kunal_Arora <aror0123@algonquinlive.com>
2024-08-20 21:48:51 -0400

    d

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const $About = document.getElementById('About');
 const $Skills = document.getElementById('Skills');
 const $Projects = document.getElementById('Projects');
 const $contacts = document.getElementById('Contact');
-
+// hello
