a2f9e4ba08f0005e1d6e052102e6c08118c2f24e
Kunal_Arora <aror0123@algonquinlive.com>
2024-09-02 08:13:31 -0400

    c

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ const $instagram = document.getElementById('instagram');
 const $X = document.getElementById('X');
 
 $github.addEventListener('click',()=>{
-  window.location.href = 'https://github.com/kunal3111';
+  window.open('https://github.com/kunal3111', '_blank');
 })
 $linkedin.addEventListener('click',()=>{
-  window.location.href = 'https://www.linkedin.com/in/kunal-arora-8a4869297/';
+  window.open('https://www.linkedin.com/in/kunal-arora-8a4869297/', '_blank');
 })
 $instagram.addEventListener('click',()=>{
-  window.location.href = 'https://www.instagram.com/kunal_00c?igsh=MWJhZml4YW91dGhobQ==';
+  window.open('https://www.instagram.com/kunal_00c?igsh=MWJhZml4YW91dGhobQ==', '_blank');
 })
 $X.addEventListener('click',()=>{
-  window.location.href = 'https://x.com/kunal_00c?t=yQHEDz1-6drs7X4_xVgaXA&s=09';
+  window.open('https://x.com/kunal_00c?t=yQHEDz1-6drs7X4_xVgaXA&s=09', '_blank');
 })
